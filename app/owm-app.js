edb893bcfc816633f9aa8471dbb645480bef325e
Melanie Burger <melanie.brgr@gmail.com>
2016-05-17 14:06:40 -0400

    cities URL parameter

--- app/owm-app.js
+++ app/owm-app.js
@@ -2,7 +2,14 @@ angular.module('OWMApp', ['ngRoute'])
 	.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/', {
 			templateUrl: 'home.html',
-            controller : 'HomeCtrl'
+            controller: 'HomeCtrl'
+		})
+		.when('/cities/:city', {
+			templateUrl: 'city.html',
+			controller: 'CityCtrl'
 		});
 	}])
-	.controller('HomeCtrl', function($scope) {});
+	.controller('HomeCtrl', function($scope) {})
+	.controller('CityCtrl', function($scope, $routeParams) {
+		$scope.city = $routeParams.city;
+	});
